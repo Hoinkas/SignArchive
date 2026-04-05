85291ee2468584c0e84dd23df0b00c2e0d30f20d
Jueltrae <hoinkas@proton.me>
2026-04-06 00:49:43 +0200

    Added filter and filtering by one tag

--- src/renderer/src/components/SearchBar/SearchBar.tsx
+++ src/renderer/src/components/SearchBar/SearchBar.tsx
@@ -1,29 +1,60 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import './SearchBar.css'
+import SearchIcon from '@renderer/assets/icons/SearchIcon'
+import FilterIcon from '@renderer/assets/icons/FilterIcon'
+import FormDropdown, { DropdownOption } from '../Form/Components/FormDropdown'
 
 interface SearchBarProps {
   searchWord: string
   setSearchWord: Dispatch<SetStateAction<string>>
+  tagOption: DropdownOption | null
+  setTagOption: Dispatch<SetStateAction<DropdownOption | null>>
 }
 
 function SearchBar(props: SearchBarProps): React.JSX.Element {
-  const { searchWord, setSearchWord } = props
+  const { searchWord, setSearchWord, tagOption, setTagOption } = props
+
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false)
+  const [tagsOptions, setTagsOptions] = useState<DropdownOption[]>([])
+
+  useEffect(() => {
+    window.api.tag.list().then((result) =>
+      setTagsOptions(
+        result.map((t) => {
+          return { id: t.id, label: t.name }
+        })
+      )
+    )
+  }, [])
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSearchWord(e.target.value.replaceAll('\u00A0', '').trimEnd())
   }
 
+  const handleFilterClick = (): void => {
+    if (isFilterOpen) setTagOption(null)
+    setIsFilterOpen(!isFilterOpen)
+  }
+
   return ( //TODO Search as form for accesibility
-    <div className="searchBar">
-      <input
-        type="search"
-        placeholder="Wyszukaj znaki.."
-        value={searchWord}
-        onChange={handleNameChange}
-      />
-      <div className="searchWrapper">
-        <div className="searchIcon" />
+    <div>
+      <div className="searchBar">
+        <input
+          type="search"
+          placeholder="Wyszukaj znaki.."
+          value={searchWord}
+          onChange={handleNameChange}
+        />
+        <div className="filterIcon" onClick={handleFilterClick}>
+          <FilterIcon />
+        </div>
+        <div className="searchWrapper">
+          <SearchIcon />
+        </div>
       </div>
+      {isFilterOpen && (
+        <FormDropdown label="" options={tagsOptions} value={tagOption} setValue={setTagOption} />
+      )}
     </div>
   )
 }
--- src/renderer/src/pages/SidePanel/ListOfWords/ListOfWords.tsx
+++ src/renderer/src/pages/SidePanel/ListOfWords/ListOfWords.tsx
@@ -1,19 +1,28 @@
 import { useWord } from '@contexts/WordContext/useWord'
 import './ListOfWords.css'
 import WordItem from './WordItem/WordItem'
+import { DropdownOption } from '@renderer/components/Form/Components/FormDropdown'
+import { WordWithCountCategories } from '@shared/types'
 
 interface ListOfWordsProps {
   searchWord: string
+  tagOption: DropdownOption | null
 }
 
-function ListOfWords(props: ListOfWordsProps): React.JSX.Element {
-  const { searchWord } = props
+function ListOfWords({ searchWord, tagOption }: ListOfWordsProps): React.JSX.Element {
   const { wordsList } = useWord()
 
-  const wordFiltered = wordsList.filter((w) =>
-    w.text.toUpperCase().includes(searchWord.toUpperCase())
-  )
-  const wordsToList = searchWord == '' ? wordsList : wordFiltered
+  function filterBySearch(words: WordWithCountCategories[]): WordWithCountCategories[] {
+    if (searchWord == '') return words
+    return words.filter((w) => w.text.toUpperCase().includes(searchWord.toUpperCase()))
+  }
+
+  function filterByTags(words: WordWithCountCategories[]): WordWithCountCategories[] {
+    if (!tagOption) return words
+    return words.filter((w) => w.categories.find((c) => c.id === tagOption.id))
+  }
+
+  const wordsToList = filterByTags(filterBySearch(wordsList))
 
   return (
     <ul className="listOfWords">
