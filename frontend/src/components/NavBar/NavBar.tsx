df1ccf4a92a9841a4230a0ebe2a5ba19f45c8cbb
Jueltrae <hoinkas@proton.me>
2026-04-09 17:42:56 +0200

    Added logo

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,7 @@
 import { usePermissions } from '@src/hooks/PermissionsContext/usePermissions'
 import './NavBar.css'
 import ThemeSwitch from '../ThemeSwitch/ThemeSwitch'
+import Logo from '@src/assets/icons/Logo.svg'
 
 function NavBar(): React.JSX.Element {
   const {isAdmin} = usePermissions()
@@ -8,8 +9,11 @@ function NavBar(): React.JSX.Element {
   return (
     <div className="navBar">
       <div className="logoTitle">
-        <a className="logo" href="/">LOGO</a>
-        <p className='titleText'>ArchiwuMig</p>
+        <img width={50} src={Logo}/>
+        <div className='titleBox'>
+          <p className='title'>Archiwu</p>
+          <p className='title purple'>Mig</p>
+        </div>
       </div>
       {isAdmin && <ThemeSwitch />}
     </div>
