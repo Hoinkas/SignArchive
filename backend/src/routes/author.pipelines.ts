87bd6c4c9b4bc5ab67cf65200f936b69698b6125
Jueltrae <hoinkas@proton.me>
2026-04-18 05:27:49 +0200

    Remove unused controllers, services, pipelines and helpers from backend

--- backend/src/routes/author.pipelines.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express'
-import { asyncHandler } from '../middlewares/middleware'
-import { authorController } from '../controllers/author.controller'
-
-export const authorRouter = Router()
-authorRouter.get('/', asyncHandler(authorController.list))
