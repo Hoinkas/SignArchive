b59bedc6cb5fd8b241965557e15eb3544f34d821
Jueltrae <hoinkas@proton.me>
2026-04-20 13:59:07 +0200

    Fixed DOM property errors

--- frontend/src/assets/icons/TextIcon.tsx
+++ frontend/src/assets/icons/TextIcon.tsx
@@ -1,7 +1,7 @@
 function TextIcon(): React.JSX.Element {
   return (
     <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 3V21M9 21H15M19 6V3H5V6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M12 3V21M9 21H15M19 6V3H5V6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
